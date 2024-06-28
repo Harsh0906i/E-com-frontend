942ee213067c077b77f9489dbbd497131469ad35
Harshit <harshitsingharya24@gmail.com>
2024-06-28 09:23:04 +0530

    updated

--- e-com/vite.config.js
+++ e-com/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://e-backend-epvh9qah9-harshit-singh-aryas-projects.vercel.app',
-        secure:false
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://e-backend-epvh9qah9-harshit-singh-aryas-projects.vercel.app',
+        secure: false
       },
     },
   },
