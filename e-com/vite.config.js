ee731d40ab0dcf96994b9ba5493038c5f55fe7f2
Harshit <harshitsingharya24@gmail.com>
2024-06-28 09:22:40 +0530

    updated

--- e-com/vite.config.js
+++ e-com/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:8080',
+        target:'https://e-backend-epvh9qah9-harshit-singh-aryas-projects.vercel.app',
         secure:false
       },
     },
