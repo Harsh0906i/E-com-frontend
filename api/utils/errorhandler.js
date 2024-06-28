be6c05e21a763288f9f520028ee3968767f5256a
Harshit <harshitsingharya24@gmail.com>
2024-06-28 13:56:22 +0530

    updated

--- api/utils/errorhandler.js
+++
@@ -1,8 +0,0 @@
-
-function errorHandler(statusCode, message, next) {
-    const err = new Error()
-    err.statusCode = statusCode;
-    err.message = message
-    return err
-}
-module.exports = errorHandler;
