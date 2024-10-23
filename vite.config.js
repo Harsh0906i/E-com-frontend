b4df808516f092501c39d70c94d0355f83655e5e
Harshit <harshitsingharya24@gmail.com>
2024-10-23 13:58:34 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    server:{
-      proxy:{
-        '/api':'http://localhost:8080'
-      },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080', // Development proxy
     },
+  },
   plugins: [react()],
 });
