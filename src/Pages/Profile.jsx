468e8393a0be18621004b363b5be9dec286d46c7
Harshit <harshitsingharya24@gmail.com>
2024-10-23 14:43:42 +0530

    updated

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -9,39 +9,51 @@ export default function Profile() {
 
   async function HandleSignOut() {
     try {
-      dispatch(signOutUserStart())
-      const res = await fetch('/api/auth/signout', {
+      dispatch(signOutUserStart());
+      const res = await fetch('http://localhost:8080/api/auth/signout', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
       });
-      const data = await res.json()
-      dispatch(signOutUserSuccess(data))
+
+      if (!res.ok) {
+        // Handle the error response
+        const errorData = await res.json();
+        dispatch(signOutUserFaliure(errorData.message));
+        return;
+      }
+
+      const data = await res.json();
+      dispatch(signOutUserSuccess(data));
     } catch (error) {
-      dispatch(signOutUserFaliure(error.message))
+      dispatch(signOutUserFaliure(error.message));
     }
+
   }
   async function HandleDelete() {
     try {
-      setDeleteState(true)
-      const res = await fetch(`/api/auth/delete/${currentUser._id}`, {
-        method: 'DELETE',
+      dispatch(signOutUserStart());
+      const res = await fetch('http://localhost:8080/api/auth/signout', {
+        method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
       });
+
       const data = await res.json();
-      if (data.success === false) {
-        dispatch(userDeleteFaliure(data.message))
-        setDeleteState(false)
+
+      // Check if the success is false and handle the error
+      if (!data.success) {
+        dispatch(signOutUserFaliure(data.message));
+        return;
       }
-      setDeleteState(false)
-      dispatch(userDeleteSuccess());
+
+      dispatch(signOutUserSuccess(data));
     } catch (error) {
-      setDeleteState(false)
-      dispatch(userDeleteFaliure(error.message))
+      dispatch(signOutUserFaliure(error.message));
     }
+
   }
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -59,9 +71,9 @@ export default function Profile() {
       </div>
       <div className="text-center text-green-700">
         <Link to={`/dashboard/${currentUser._id}`}>
-        <button className="bg-green-200 text-green-800 py-2 px-3 rounded-full border border-green-700  hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 transition">
-          Dashboard
-        </button>
+          <button className="bg-green-200 text-green-800 py-2 px-3 rounded-full border border-green-700  hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 transition">
+            Dashboard
+          </button>
         </Link>
       </div>
     </div>
