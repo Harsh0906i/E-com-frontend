68d033b9326d8e450f4f3a16589a6565961fff49
Harshit <harshitsingharya24@gmail.com>
2024-10-24 10:46:58 +0530

    updated

--- src/Pages/ShowProduct.jsx
+++ src/Pages/ShowProduct.jsx
@@ -16,8 +16,9 @@ export default function ShowProduct() {
     const { cart } = useSelector((state) => state.user1)
     const dispatch = useDispatch()
     const [loading, setloading] = useState(false);
+    const [token, settoken] = useState(false);
     const [item, setitem] = useState({});
-    const [Copied, setCopied] = useState(false)
+    const [tokenLoading, settokenLoading] = useState(false)
     const [err, seterr] = useState(false);
     const params = useParams();
     const [open, setOpen] = React.useState(false);
@@ -27,6 +28,7 @@ export default function ShowProduct() {
         async function fetchItem() {
             try {
                 // Make the fetch request
+                setloading(true)
                 const res = await fetch(`http://localhost:8080/api/item/getItem/${params.itemid}`);
                 const data = await res.json(); // Parse the JSON response
 
@@ -82,60 +84,80 @@ export default function ShowProduct() {
             </IconButton>
         </React.Fragment>
     );
+    function handleToken(e) {
+        e.preventDefault();
+        settokenLoading(true)
+        setTimeout(() => {
+            settoken('Token is invalid!')
+        }, 2000)
+        settokenLoading(false)
+    }
 
     return (
         <>
-            <div className=" justify-center">
-                <Card sx={{ maxWidth: 345 }} className="mx-auto mt-8" >
-                    <CardMedia
-                        sx={{ height: 250 }}
-                        image={item.image}
-                        title={item.name}
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {item.name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
+            {loading ? <h1>Loading...</h1> : (
+                <div>
+                    <div className=" justify-center">
+                        <Card sx={{ maxWidth: 345 }} className="mx-auto mt-8" >
+                            <CardMedia
+                                sx={{ height: 250 }}
+                                image={item.image}
+                                title={item.name}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {item.name}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
 
-                            {item.storage && (
-                                <>
-                                    <div>
-                                        RAM: {item.storage.RAM} GB
-                                    </div>
-                                    <div>
-                                        ROM: {item.storage.ROM >= 1024 ? `${item.storage.ROM / 1024}TB` : `${item.storage.ROM} GB`}
-                                    </div>
-                                </>
-                            )}
-                            <Typography component="span" variant="body2" color="text.secondary">
-                                {item.discountedPrice ? (
-                                    <>
-                                        Regular Price: <span className="line-through">₹{new Intl.NumberFormat('en-IN').format(item.regularPrice)}</span>
-                                        <div>
-                                            Discounted Price: ₹{new Intl.NumberFormat('en-IN').format(item.discountedPrice)}
-                                        </div>
-                                    </>
-                                ) : (
-                                    <>
-                                        Price: <span>₹{new Intl.NumberFormat('en-IN').format(item.regularPrice)}</span>
-                                    </>
-                                )}
-                            </Typography>
-                        </Typography>
-                    </CardContent>
-                    <Button className="">{currentUser ? <button onClick={() => { { addToCart() }; handleClick() }}>Add to cart</button> : <Link className="text-red-700 " to={'/login'}>Sign in to add to cart</Link>}</Button>
-                </Card>
-            </div>
-            <div>
-                <Snackbar className='text-white'
-                    open={open}
-                    autoHideDuration={2000}
-                    onClose={handleClose}
-                    message="Item added to cart!"
-                    action={action}
-                />
-            </div>
+                                    {item.storage && (
+                                        <>
+                                            <div>
+                                                RAM: {item.storage.RAM} GB
+                                            </div>
+                                            <div>
+                                                ROM: {item.storage.ROM >= 1024 ? `${item.storage.ROM / 1024}TB` : `${item.storage.ROM} GB`}
+                                            </div>
+                                        </>
+                                    )}
+                                    <Typography component="span" variant="body2" color="text.secondary">
+                                        {item.discountedPrice ? (
+                                            <>
+                                                Regular Price: <span className="line-through">₹{new Intl.NumberFormat('en-IN').format(item.regularPrice)}</span>
+                                                <div>
+                                                    Discounted Price: ₹{new Intl.NumberFormat('en-IN').format(item.discountedPrice)}
+                                                </div>
+                                            </>
+                                        ) : (
+                                            <>
+                                                Price: <span>₹{new Intl.NumberFormat('en-IN').format(item.regularPrice)}</span>
+                                            </>
+                                        )}
+                                    </Typography>
+                                </Typography>
+                            </CardContent>
+                            <Button className="">{currentUser ? <button onClick={() => { { addToCart() }; handleClick() }}>Add to cart</button> : <Link className="text-red-700 " to={'/login'}>Sign in to add to cart</Link>}</Button>
+                        </Card>
+                    </div>
+                    <div>
+                        <form>
+                            <label>Apply token for discount!</label>
+                            <input type="text" placeholder="TOKEN..." />
+                            <button onClick={handleToken} type="submit" >{tokenLoading?'Applying...':'Apply'}</button>
+                        </form>
+                        {token && <p className="text-red-500 text-center">{token}</p>}
+                    </div>
+                    <div>
+                        <Snackbar className='text-white'
+                            open={open}
+                            autoHideDuration={2000}
+                            onClose={handleClose}
+                            message="Item added to cart!"
+                            action={action}
+                        />
+                    </div>
+                </div>
+            )}
         </>
     );
 }
