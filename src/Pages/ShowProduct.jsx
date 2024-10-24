8c4af804d031830a7874df5821025e764cdd5814
Harshit <harshitsingharya24@gmail.com>
2024-10-24 09:31:07 +0530

    updated

--- src/Pages/ShowProduct.jsx
+++ src/Pages/ShowProduct.jsx
@@ -29,7 +29,7 @@ export default function ShowProduct() {
             try {
                 const res = await fetch(`http://localhost:8080/api/item/getItem/${params.itemid}`);
                 const data = await res.json();
-                if (data.success === false) {
+                if (!data.ok) {
                     setloading(false);
                     return;
                 }
