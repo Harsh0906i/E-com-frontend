8a01634949c00a724f06c1ee56c9f7b7728e1659
Harshit <harshitsingharya24@gmail.com>
2024-10-26 09:46:49 +0530

    updated

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -82,8 +82,8 @@ export default function Profile() {
         
         {currentUser.isAdmin === 'true' && (
           <Link to={`/Admin/${currentUser._id}`}>
-            <button className="bg-green-200 text-green-800 py-2 px-3 rounded-full border border-green-700  hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 transition">
-              Admin Login
+            <button className="bg-green-200 text-green-800 py-2 px-3 rounded-full border border-green-700  hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 transition mx-4">
+              Requested Products
             </button>
           </Link>
         )}
